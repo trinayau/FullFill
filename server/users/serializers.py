f94b4f568c4e215066b09afb2780d58888006b8f
Trina <92634994+trinayau@users.noreply.github.com>
2022-06-05 11:22:52 +0100

    add user/register endpoint to django

---
+++ server/users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from users.models import NewUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NewUser
+        fields = ('email', 'user_name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        # as long as the fields are the same, we can just use this
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
