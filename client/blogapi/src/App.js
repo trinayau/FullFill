2131e598c62eb9c6fbc9488bff46c4e91626f279
doreenkam <dkamushinda@gmail.com>
2022-06-06 17:58:16 +0100

    Added new content

--- client/blogapi/src/App.js
+++ client/blogapi/src/App.js
@@ -1,28 +1,37 @@
-import { ThemeProvider } from '@mui/material';
-import React from 'react';
-import './App.css';
-import {theme} from './theme';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Footer, Header, Homepage, Login, Logout, Register } from './components';
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import "./App.css";
+import { theme } from "./theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Footer,
+  Header,
+  Homepage,
+  Login,
+  Logout,
+  Locator,
+  Register,
+  Donation,
+} from "./components";
 const App = () => {
-
-
   return (
     <div className="App">
-       <ThemeProvider theme={theme}>
-    <Router>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/register" element={<Register />}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/logout" element={<Logout />}/>
-    </Routes>
-    <Footer/>
-    </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/locator" element={<Locator />} />
+            <Route path="/donation" element={<Donation />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+          <Footer />
+        </Router>
       </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
