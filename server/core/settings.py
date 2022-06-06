2e53caba52578eede2a5ea5fc7232b8444a1cbf5
Trina <92634994+trinayau@users.noreply.github.com>
2022-06-06 16:24:52 +0100

    create custom token view for auth

--- server/core/settings.py
+++ server/core/settings.py
@@ -160,8 +160,8 @@ AUTH_USER_MODEL =  "users.NewUser"
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
