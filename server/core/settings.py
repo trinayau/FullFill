f5a739cebabb653f4a863a56c45bf1d41357b1dc
Trina <92634994+trinayau@users.noreply.github.com>
2022-06-07 23:59:43 +0100

    heroku procfile

--- server/core/settings.py
+++ server/core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -186,3 +188,4 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+django_heroku.settings(locals())
