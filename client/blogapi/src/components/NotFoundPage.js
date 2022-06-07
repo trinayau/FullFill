b672c8ebb3cf79ca2c571efcc9ff09ce75466838
“scralfie” <kellyalfie6@gmail.com>
2022-06-07 11:35:43 +0100

    erro 404 page

--- client/blogapi/src/components/NotFoundPage.js
+++ client/blogapi/src/components/NotFoundPage.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./err404.css";
+
+
+
+
+
 const NotFoundPage = () => {
-    return ( <h1>oh noes. go back?</h1> );
+
+        const navigate = useNavigate();
+      
+        function handleClick() {
+          navigate(-1);
+        }
+
+
+    return (<>
+        <div className="err_page">
+            <div className="err_page_left">
+                <img src={require('./err_404.png')} width="360px" height="250px" />
+            </div>
+            <div className="err_page_right">
+                <h1>404</h1> <br></br>
+                <h4>OOPS. Looks like the page you're looking for does not exist</h4> <br></br>
+                <p>Don't worry. Since you're valuable to us we will bring you back to safety</p> <br></br>
+                <button onClick={handleClick} className="err_btn">Back</button> <br></br> <br></br>
+                <h3 className="credit">Click here if you would like to <a href="/">Login</a> or here if you would like to <a href="/register">register</a></h3>
+            </div>
+        </div>
+    </>);
 }
- 
+
 export default NotFoundPage;
