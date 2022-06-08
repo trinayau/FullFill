58972565a2a1d247652bf3ed9cb521e8bbd1f78e
doreenkam <dkamushinda@gmail.com>
2022-06-08 17:48:28 +0100

    Added recipes/meal components

--- client/blogapi/src/components/Recipes/index.js
+++ client/blogapi/src/components/Recipes/index.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import MealList from "../MealList";
 
-const Recipes = () => {
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=fd6746bcbe074d678f49571ae82b2c27&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
   return (
-    <>
-      <h1>Recipes</h1>
-    </>
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
   );
-};
+}
 
-export default Recipes;
+export default App;
